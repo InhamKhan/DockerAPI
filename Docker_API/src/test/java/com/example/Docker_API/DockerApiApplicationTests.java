3ca33a820f4e60822e39763c71f343331523bc8d
InhamKhan <inham@gmail.com>
2024-12-10 16:23:37 +0500

    docker_api

---
+++ Docker_API/src/test/java/com/example/Docker_API/DockerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Docker_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
